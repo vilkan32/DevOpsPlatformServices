9d8e15d19147cc8032c0d1b54b84246f91ab7510
vlyubenovbteam <95635227+vlyubenovbteam@users.noreply.github.com>
2022-07-18 21:17:31 +0300

    edit appsettings for the token

--- DevOpsPlatform/Controllers/HomeController.cs
+++ DevOpsPlatform/Controllers/HomeController.cs
@@ -11,9 +11,15 @@ namespace DevOpsPlatform.Controllers
     public class HomeController : ControllerBase
     {
         private HttpClient _httpClient;
-        public HomeController(HttpClient httpClient)
+
+        private IConfiguration _configuration;
+
+        private readonly string _token;
+        public HomeController(HttpClient httpClient, IConfiguration configuration)
         {
             _httpClient = httpClient;
+            _configuration = configuration;
+            _token =_configuration.GetSection("Github").GetSection("Token").Value;
         }
 
         [HttpPost]
@@ -30,14 +36,12 @@ namespace DevOpsPlatform.Controllers
                 var parsedResponse = JsonConvert.DeserializeObject<Payload>(body);
 
                 _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github+json"));
-                _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("token", "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");
+                _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("token", _token);
                 _httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("vilkan32");
 
                 using var request = new HttpRequestMessage(HttpMethod.Post, $"https://api.github.com/repos/vilkan32/DevOpsPlatformServices/issues/{parsedResponse.Issue.Number}/comments");
 
-                var commentMessage = "Thanks for submitting this issue. Our Team will get back to you as soon as possible!";
-
-                request.Content = JsonContent.Create(new { body = commentMessage });
+                request.Content = JsonContent.Create(new { body = "Thanks for submitting this issue. Our Team will get back to you as soon as possible!" });
 
                 var response = await _httpClient.SendAsync(request);
 
