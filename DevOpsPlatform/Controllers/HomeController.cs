ab4e9815a7fd8dc8886e7698c4f3b0222d7a673e
vlyubenovbteam <95635227+vlyubenovbteam@users.noreply.github.com>
2022-07-18 19:16:48 +0300

    log and check if payload is being displayed

---
+++ DevOpsPlatform/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevOpsPlatform.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpPost]
+        [Route("Webhook")]
+        public IActionResult TriggerWebhook(string jsonPayload)
+        {
+            Console.WriteLine("!!!!!!!!!!!!!!!!!!!!");
+            Console.WriteLine(jsonPayload);
+            Console.WriteLine("!!!!!!!!!!!!!!!!!!!!");
+            return Ok(jsonPayload);
+        }
+    }
+}
